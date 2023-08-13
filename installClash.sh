32a07d672bdb158f74718989b5e8d200a51a9fc3
aweftr <aweftger@gmail.com>
2023-08-13 12:45:41 +0800

    Add country mmdb to prevent clash init error

--- installClash.sh
+++ installClash.sh
@@ -5,8 +5,8 @@ chmod +x clash
 echo "clash version"
 ./clash -v
 git clone https://github.com/haishanh/yacd.git
-mkdir config
-mkdir log
+if [ ! -d "config" ]; then mkdir config; fi
+if [ ! -d "log" ]; then mkdir log; fi
 curl -sSL $CLASH_URL -o config/config.yaml
 cd yacd
 git switch gh-pages
